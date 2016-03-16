84c55a0dc0951bce9bee007627161844335302fa
ProditorMagnus <public.ravana@gmail.com>
2016-03-16 14:33:43 +0200

    Tick testing

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("this is main class");
 
-        Application app = new Application();
-        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        app.setVisible(true);
+        Tick tick = new Tick();
+
+//        Application app = new Application();
+//        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        app.setVisible(true);
     }
 }
--- src/Tick.java
+++ src/Tick.java
@@ -4,26 +4,28 @@
 public class Tick {
 
     long lastMoment = System.nanoTime();
-    long tickLength = 500000000L;
+    long tickLength = 500000000;
     boolean playing = true;
-    double unprocessed = 0;
+    double unprocessed = 0.0;
 
     public Tick() {
 
         while (playing) {
             long thisMoment = System.nanoTime();
-            unprocessed += (thisMoment - lastMoment) / tickLength;
+            //System.out.println(thisMoment-lastMoment);
+            unprocessed += (thisMoment - lastMoment);
             lastMoment = thisMoment;
-            boolean shouldRender = true;
+            boolean shouldRender = false;
 
-            while (unprocessed >= 1) {
+            while (unprocessed/tickLength >= 1) {
                 //tick();
-                unprocessed -= 1;
+                unprocessed -= tickLength;
                 shouldRender = true;
             }
 
             if (shouldRender) {
                 //application.repaint();
+                System.out.println("rendering at "+System.currentTimeMillis());
             }
         }
     }
