4811ba586ec20b719a8558131d2546f547acc799
ProditorMagnus <public.ravana@gmail.com>
2016-03-19 13:56:18 +0200

    Basic WorldBuilding and scrolling

--- src/screen/PlayScreen.java
+++ src/screen/PlayScreen.java
@@ -6,27 +6,56 @@ import java.util.List;
 import action.GameAction;
 import action.Movement;
 import asciiPanel.AsciiPanel;
+import world.World;
+import world.WorldBuilder;
 
 public class PlayScreen implements Screen {
+    private World world;
+    private int screenWidth = 80;
+    private int screenHeight = 24;
+    private int viewX = 0;
+    private int viewY = 0;
+
+    public PlayScreen() {
+        createWorld();
+    }
 
     public void displayOutput(AsciiPanel terminal) {
-        terminal.write("You are having fun.", 1, 1);
-        terminal.writeCenter("-- press [escape] to lose or [enter] to win --", 22);
+        displayTiles(terminal,viewX,viewY);
     }
 
     public Screen respondToUserInput(KeyEvent key) {
         switch (key.getKeyCode()){
             case KeyEvent.VK_ESCAPE: return new LoseScreen();
             case KeyEvent.VK_ENTER: return new WinScreen();
-            case KeyEvent.VK_LEFT:
-                Movement movement = new Movement(0,-1);
-                //AddEvent();
+            case KeyEvent.VK_LEFT: viewX = viewX<2-screenWidth ? viewX : viewX-1; break;
+            case KeyEvent.VK_RIGHT: viewX = viewX>world.width()-2 ? viewX : viewX+1; break;
+            case KeyEvent.VK_UP: viewY = viewY<2-screenHeight ? viewY : viewY-1; break;
+            case KeyEvent.VK_DOWN: viewY = viewY>world.height()-2 ? viewY : viewY+1; break;
         }
 
         return this;
     }
 
+    private void createWorld(){
+        world = new WorldBuilder(90, 31)
+                .makeCaves()
+                .build();
+    }
+
     public void AddEvent(List<List<GameAction>> events, GameAction action){
         events.get(0).add(action);
+    }
+
+    private void displayTiles(AsciiPanel terminal, int left, int top) {
+        for (int x = 0; x < screenWidth; x++){
+            for (int y = 0; y < screenHeight; y++){
+                int wx = x + left;
+                int wy = y + top;
+
+                terminal.write(world.glyph(wx, wy), x, y, world.color(wx, wy));
+            }
+        }
+        //terminal.write(player.glyph(), player.x - left, player.y - top, player.color());
     }
 }
