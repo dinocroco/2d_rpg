53ff83a04a9b71b31faf8178cd85d52a5e39e4d2
dinocroco <ruusmannlaura@gmail.com>
2016-03-22 21:26:01 +0200

    player action management

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,9 +1,9 @@
 package player;
 
 import action.GameAction;
-import screen.Screen;
-import screen.StartScreen;
+import asciiPanel.AsciiPanel;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +11,21 @@ public class Player {
 
     public int x;
     public int y;
+    public final char glyph = (char)254;
+    public final Color color = AsciiPanel.cyan;
 
-    List<GameAction> actions = new ArrayList<>();
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public List<GameAction> actions = new ArrayList<>();
+
+    public List<GameAction> getActions(){
+        List<GameAction> newActions = new ArrayList<>(actions);
+        actions.clear();
+        return newActions;
+    }
 
     public void doAction(GameAction action){
         actions.add(action);
